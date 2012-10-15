934ce596ad105eb4dfd200eee8b29d1b1b1e4edf
Zachary Hamed <zhamed@college.harvard.edu>
2012-10-14 21:05:19 -0400

    Added README

---
+++ index.php
@@ -0,0 +1,22 @@
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Zach Hamed ♥ CODE2040</title>
+        <meta name="description" content="A fun project.">
+        <meta name="author" content="Zachary Hamed">
+
+        <link href="css/main.css" rel="stylesheet">
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
+        <script src="js/waypoints.min.js"></script>
+    </head>
+    
+    <body>
+        <div class="laptop-body">
+            <img src="img/mac.png" width="100%" />
+            <div class="viewport"><img src="img/screen.png" width="100%" /></div>
+
+        </div>
+    </body>
+    
+</html>
