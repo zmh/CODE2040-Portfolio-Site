c6bf47fdcf94542d879b155a6499494bf265bbf1
Zachary Hamed <zhamed@college.harvard.edu>
2012-10-16 13:32:38 -0400

    Removed scrolling functionality and added introduction screen.

--- index.php
+++ index.php
@@ -5,18 +5,49 @@
         <meta name="description" content="A fun project.">
         <meta name="author" content="Zachary Hamed">
 
+        <link href='http://fonts.googleapis.com/css?family=Lobster+Two:400,400italic' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Droid+Sans' rel='stylesheet' type='text/css'>  
+        <script src="http://use.edgefonts.net/arvo.js"></script>
+        <script src="http://use.edgefonts.net/amaranth.js"></script>
+      
+
         <link href="css/main.css" rel="stylesheet">
 
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
-        <script src="js/waypoints.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+        <script src="js/jquerytypewriter.js"></script>
+
+
     </head>
     
     <body>
+        <div class="intro">
+            A fun presentation in CSS and Javascript for
+        </div>
+        <div class="logo">
+            <img class="logo-arrow" src="img/code2040-arrow.png" height="40px;" />
+            <img class="logo-main" src="img/code2040-logo.png" height="40px;" />
+        </div>
+        
+        <a class="button-tumblr" href="#">Let's go!</a>
+
+
         <div class="laptop-body">
             <img src="img/mac.png" width="100%" />
-            <div class="viewport"><img src="img/screen.png" width="100%" /></div>
-
+            <div class="viewport"><img src="img/screen2.png" width="100%" /></div>
         </div>
+        
+        <div class="hideme finder">
+            <img src="img/window.png" />
+            <div class="finder-text">Hi hi hi test test test</div>
+        </div>
+        
+        <div class="hideme chrome">
+            <img src="img/chrome.png" />
+            <div class="chrome-text">www.google.com</div>
+        </div>
+
     </body>
     
+    <script src="js/main.js"></script>
+
 </html>
